9eb55f52014d1e7a81ef0fe1b50881cd79997911
Dmitry Tarasov <vizalyyy@gmail.com>
2025-04-21 03:29:31 +0500

    With timeout

--- gh.test.js
+++ gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
     page = await browser.newPage();
-    await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,20 +9,24 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+    beforeEach(async () => {
+        await page.goto("https://github.com/team");
+    });
+
     test("The h1 header content'", async () => {
         const firstLink = await page.$("header div div a");
         await firstLink.click();
         await page.waitForSelector("h1");
         const title2 = await page.title();
         expect(title2).toEqual(
-            "GitHub: Where the world builds software · GitHub"
+            "GitHub · Build and ship software on a single, collaborative platform · GitHub"
         );
-    });
+    }, 7000);
 
     test("The first link attribute", async () => {
         const actual = await page.$eval("a", link => link.getAttribute("href"));
         expect(actual).toEqual("#start-of-content");
-    });
+    }, 1000);
 
     test("The page contains Sign in button", async () => {
         const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -31,6 +34,6 @@ describe("Github page tests", () => {
             visible: true
         });
         const actual = await page.$eval(btnSelector, link => link.textContent);
-        expect(actual).toContain("Sign up for free");
-    });
+        expect(actual).toContain("Get started with Team");
+    }, 500);
 });
--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     launch: {
-        slowMo: 1000,
         headless: false,
         defaultViewport: null,
         args: ["--start-maximized"] //— используем максимальный размер окна браузера
